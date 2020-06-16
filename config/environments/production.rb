ac9172017c593e74edf1a290e4d5daa2a9fad03f
Armando Murga <armando.murga@gusto.com>
2020-06-15 22:53:16 -1000

    Trying to remove cookies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.middleware.delete ActionDispatch::Cookies
+  config.middleware.delete ActionDispatch::Session::CookieStore
+
+  Rails.application.config.session_store :disabled
+
 end
